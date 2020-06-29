d6c3729c67d27a8bb34ea43b9d3f4267a950257c
raghavbobal1 <raghavbobal001@gmail.com>
2020-06-29 14:44:16 -0400

    added a file to manage todo items

---
+++ todo_Raghav_C0780996/TodoItem.swift
@@ -0,0 +1,15 @@
+//
+//  TodoItem.swift
+//  todo_Raghav_C0780996
+//
+//  Created by Raghav Bobal on 2020-06-29.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TodoItem {
+    var todoTitle: String!
+    
+}
